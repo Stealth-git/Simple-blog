f075456a2fde6d363746939ccce3aa2a045b5a58
Stealth-git <rebill2me@gmail.com>
2021-03-28 21:09:19 +0300

    Impliment artilces#show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,10 @@ class ArticlesController < ApplicationController
   def new
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def create
     # вывод хеша params[:article] на экран 
     # render plain: params[:article].inspect
