be9addd858e60e84b41798f8a15d4f4342e9f49e
Stealth-git <rebill2me@gmail.com>
2021-03-28 20:45:02 +0300

    Implement articles#create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,22 @@ class ArticlesController < ApplicationController
 
   def create
     # вывод хеша params[:article] на экран 
-    render plain: params[:article].inspect
+    # render plain: params[:article].inspect
+
+    @article = Article.new(article_params)
+    if @article.valid?
+      @article.save
+    else
+      # возвращаем форму через action: 'new'
+      render action: "new"
+
+    end
   end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
 end
