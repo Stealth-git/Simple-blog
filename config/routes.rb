5ecd70de028773e337151f244189bbfb08a58ce3
Stealth-git <rebill2me@gmail.com>
2021-03-28 19:16:30 +0300

    Add terms controller and show.html.erb view

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   resource :contacts, only: [:new, :create], path_names: { :new => '' }
   resources :articles
+  resource :terms
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
