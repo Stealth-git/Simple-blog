f92bed687bfd4afa12aa29c220f13159df6c581f
Stealth-git <rebill2me@gmail.com>
2021-04-24 15:47:10 +0300

    Update comment spec (validate_length_of)

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,15 @@
 require "rails_helper"
 
 describe Comment do
-  it { should belong_to :article }
+  describe "validations" do
+    it { should validate_presence_of :body }
+  end
+
+  describe "associations" do
+    it { should belong_to :article }
+  end
+
+  describe "lengths" do
+    it { should validate_length_of :body }
+  end
 end
